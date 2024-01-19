7049fa7bf9c462dc8a11c0b355461e98391eebcb
saviomds <dominiquesavio2003@gmail.com>
2024-01-19 19:43:14 +0200

    update 404.html, style.css, home.html. main.js

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+const message = document.querySelector("#message");
+const message2 = document.querySelector("#message2");
+
+message.onclick = () => {
+  alert("You Are not Allowed, Please try Again");
+};
+message2.onclick = () => {
+  alert("You Are not Allowed, Please try Again");
+};
