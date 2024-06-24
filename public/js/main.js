fb92123e58253cacb1f2ccc142ae0ad3627442bc
saviomds <dominiquesavio2003@gmail.com>
2024-06-24 19:50:49 +0200

    add readmore

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,26 @@ message.onclick = () => {
 message2.onclick = () => {
   alert("You Are not Allowed, Please try Again");
 };
+const descriptionElement = document.querySelector(".description");
+
+const description = descriptionElement.innerHTML;
+
+const words = description.split(" ");
+
+const maxWords = 40;
+const shortDescription = words.slice(0, maxWords).join(" ");
+
+const link = document.createElement("a");
+const text = document.createTextNode("read more");
+link.appendChild(text);
+link.href = "#readmore";
+
+link.addEventListener("click", (e) => {
+  e.preventDefault();
+  const maxWords = description.length;
+  const shortDescription = words.slice(0, maxWords).join(" ");
+  descriptionElement.innerHTML = shortDescription;
+});
+
+descriptionElement.innerHTML = shortDescription + "... ";
+descriptionElement.appendChild(link);
